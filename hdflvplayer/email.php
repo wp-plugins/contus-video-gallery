a49d81a6db54af10d112f0e0d1e15d2d89198af1
hdflvplayer <hdflvplayer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-13 13:59:20 +0000

    Update videogallery version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/contus-video-gallery/tags/2.0@712397 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hdflvplayer/email.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Name: Wordpress Video Gallery
+Plugin URI: http://www.apptha.com/category/extension/Wordpress/Video-Gallery
+Description: email file for player.
+Version: 2.0
+Author: Apptha
+Author URI: http://www.apptha.com
+License: GPL2
+*/
+
+$to = $_POST['to'];
+$from = $_POST['from'];
+$url = $_POST['url'];
+$subject = $_POST['Note'];
+ 
+
+// variables are sent to this PHP page through
+// the POST method.  POST is a global associative array
+// of variables passed through this method.  From that, we
+// can get the values sent to this page from Flash and
+// assign them to appropriate variables which can be used
+// in the PHP mail() function.
+
+// header information not including sendTo and Subject
+// these all go in one variable.  First, include From:
+$headers = "From: "."<" . $_POST['from'] .">\r\n";
+// next include a replyto
+$headers .= "Reply-To: " . $_POST['from'] . "\r\n";
+// often email servers won't allow emails to be sent to
+// domains other than their own.  The return path here will
+// often lift that restriction so, for instance, you could send
+// email to a hotmail account. (hosting provider settings may vary)
+// technically bounced email is supposed to go to the return-path email
+$headers .= "Return-path: " . $_POST['from'];
+
+// now we can add the content of the message to a body variable
+
+$message = $_POST['Note'] . "\n\n";
+$message .= "Video URL: " . $url;
+
+
+// once the variables have been defined, they can be included
+// in the mail function call which will send you an email
+if(mail($to, $subject, $message, $headers))
+{
+	echo "output=sent";
+} else {
+	echo "output=error";
+}
+
+?>
