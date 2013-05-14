cc0825c06e9300348c1a618a957d3e72a1cc0e24
hdflvplayer <hdflvplayer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-14 05:07:06 +0000

    Update videogallery version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/contus-video-gallery/tags/2.0@712707 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- install.php
+++ install.php
@@ -8,7 +8,7 @@ Author: Apptha
 Author URI: http://www.apptha.com
 License: GPL2
 */
-
+echo 'jsd';exit;
 function AddColumnIfNotExists($errorMsg, $table, $column, $attributes = "INT( 11 ) NOT NULL DEFAULT '0'") {
      global $wpdb;
     $columnExists = false;
